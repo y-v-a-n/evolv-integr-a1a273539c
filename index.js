bbe9ee8db2b7b71729a47fa32d8b3df8d57c4a9e
y-v-a-n <yvanfrey@icloud.com>
2023-07-10 19:23:28 -0700

    cleanup

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 module.exports = function(config) {
-    console.log('get-visitor integration ran');
+    console.log('a1a273539c integration ran');
     console.log('with this config file: \n' + config);
     alloy("getIdentity").then(function (result) {
         console.log("result:", result);
         console.log("result ECID:", result.identity.ECID);
         console.log("result RegionId:", result.edge.regionId);
+        // evolv.context.set('vzOmni.visitorId',result.identity.ECID);
     }).catch(function (error) {
         // The command failed.
         // "error" will be an error object with additional information.
